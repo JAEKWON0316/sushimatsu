cd93ca962a5da61f3e06172efa781881789e9f56
innocurve <hjpw96@naver.com>
2025-04-01 18:33:29 +0900

    배포

--- components/home/HeroSection.tsx
+++ components/home/HeroSection.tsx
@@ -1,8 +1,6 @@
 "use client"
 
-import { useRef } from "react"
 import Image from "next/image"
-import { Button } from "@/components/ui/button"
 
 export default function HeroSection() {
   return (
